a75c4005b9ebaa1d3b1aa6eff0811ba6b3e61157
Ibo <info@iboibelings.nl>
2025-04-18 14:21:42 +0200

    aadded caching

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'video-cache';
+const CACHE_FILES = [];
+
+// Install event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+// Fetch event: Serve cached files if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
